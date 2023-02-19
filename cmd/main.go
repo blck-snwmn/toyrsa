957a0cb02ed7e84e3c5503d2d57d565ac96ea68d
blck-snwmn <whitesnowmancreator@gmail.com>
2023-02-20 02:41:35 +0900

    Impl EncryptPKCS1v15

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"crypto/rsa"
 	"fmt"
+	"io"
 	"math/big"
 	"reflect"
 )
@@ -18,13 +20,49 @@ func main() {
 
 		plaintext = []byte("Cozy lummox gives smart squid who asks for job pen.")
 	)
+	fmt.Println(key.PublicKey.Size())
 
-	// crypto
-	ciphertext := Encrypt(n, big.NewInt(int64(e)), plaintext)
-	fmt.Printf("%X\n", plaintext)
-	decryptPlaintext := Decrypt(n, d, ciphertext)
-	fmt.Printf("%X\n", decryptPlaintext)
-	fmt.Printf("%t\n", reflect.DeepEqual(plaintext, decryptPlaintext))
+	{
+		ciphertext := Encrypt(n, big.NewInt(int64(e)), plaintext)
+		fmt.Printf("%X\n", plaintext)
+		decryptPlaintext := Decrypt(n, d, ciphertext)
+		fmt.Printf("%X\n", decryptPlaintext)
+		fmt.Printf("%t\n", reflect.DeepEqual(plaintext, decryptPlaintext))
+
+	}
+	{
+		dummy := bytes.Repeat([]byte{0xFF}, 128)
+		gociphertext, _ := rsa.EncryptPKCS1v15(bytes.NewBuffer(dummy), &key.PublicKey, plaintext)
+		fmt.Printf("%X\n", gociphertext)
+
+		ciphertext, _ := EncryptPKCS1v15(bytes.NewBuffer(dummy), n, big.NewInt(int64(e)), plaintext)
+		fmt.Printf("%X\n", ciphertext)
+	}
+}
+
+func fillNonZeroBytes(random io.Reader, buf []byte) error {
+	for i := range buf {
+		for buf[i] == 0 {
+			_, err := random.Read(buf[i : i+1])
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func EncryptPKCS1v15(random io.Reader, n, e *big.Int, plaintext []byte) ([]byte, error) {
+	k := (n.BitLen() + 7) / 8
+	em := make([]byte, k)
+	em[1] = 2
+	err := fillNonZeroBytes(random, em[2:k-len(plaintext)-1]) // -1 is 0x00
+	if err != nil {
+		return nil, err
+	}
+
+	copy(em[k-len(plaintext):], plaintext)
+	return Encrypt(n, e, em), nil
 }
 
 func Encrypt(n, e *big.Int, plaintext []byte) []byte {
