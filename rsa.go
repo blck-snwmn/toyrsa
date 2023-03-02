0f0e0677f8db297ee72c0c41f6cc65778e6921c2
blck-snwmn <whitesnowmancreator@gmail.com>
2023-03-03 02:54:42 +0900

    Extract oaep

--- rsa.go
+++ rsa.go
@@ -49,59 +49,6 @@ func DecryptPKCS1v15(n, d *big.Int, ciphertext []byte) ([]byte, error) {
 	return em[index+1:], nil
 }
 
-func EncryptOAEP(hash hash.Hash, random io.Reader, n, e *big.Int, plaintext, label []byte) ([]byte, error) {
-	hash.Reset()
-
-	hash.Write(label)
-	lHash := hash.Sum(nil)
-	hash.Reset()
-
-	k := (n.BitLen() + 7) / 8
-	em := make([]byte, k)
-
-	seed := em[1 : hash.Size()+1]
-	db := em[hash.Size()+1:]
-
-	copy(db[:hash.Size()], lHash)
-
-	db[len(db)-len(plaintext)-1] = 0x01
-	copy(db[len(db)-len(plaintext):], plaintext)
-
-	_, err := random.Read(seed)
-	if err != nil {
-		return nil, err
-	}
-	mgf1xor(db, seed, hash)
-	mgf1xor(seed, db, hash)
-
-	return Encrypt(n, e, em), nil
-}
-
-func DecryptOAEP(hash hash.Hash, random io.Reader, n, d *big.Int, ciphertext, label []byte) ([]byte, error) {
-	em := Decrypt(n, d, ciphertext)
-
-	hash.Reset()
-	hash.Write(label)
-	lHash := hash.Sum(nil)
-	hash.Reset()
-
-	seed := em[1 : hash.Size()+1]
-	db := em[hash.Size()+1:]
-
-	mgf1xor(seed, db, hash)
-	mgf1xor(db, seed, hash)
-
-	isSameHash := subtle.ConstantTimeCompare(db[:hash.Size()], lHash) == 1
-
-	db = db[hash.Size():]
-	index := bytes.Index(db, []byte{0x01}) // no constant time
-	valid := index != -1 && isSameHash && subtle.ConstantTimeByteEq(em[0], 0x00) == 1
-	if !valid {
-		return nil, errors.New("invalid data")
-	}
-	return db[index+1:], nil
-}
-
 func Encrypt(n, e *big.Int, plaintext []byte) []byte {
 	return encrypt(n, e, plaintext)
 }
--- rsa_test.go
+++ rsa_test.go
@@ -83,35 +83,3 @@ func Test_EncryptPKCS1v15(t *testing.T) {
 		}
 	}
 }
-
-func Test_EncryptOAEP(t *testing.T) {
-	t.Parallel()
-	key, _ := rsa.GenerateKey(rand.Reader, 1024)
-
-	var (
-		d = key.D
-		n = key.N
-		e = big.NewInt(int64(key.E))
-
-		plaintext = []byte("Cozy lummox gives smart squid who asks for job pen.")
-	)
-	label := []byte("test")
-
-	reader := rand.Reader
-	b := bytes.NewBuffer(nil)
-	reader = io.TeeReader(reader, b)
-	for i := 0; i < 1000; i++ {
-		gociphertext, _ := rsa.EncryptOAEP(sha256.New(), reader, &key.PublicKey, plaintext, label)
-		ciphertext, _ := EncryptOAEP(sha256.New(), b, n, e, plaintext, label)
-		if !reflect.DeepEqual(ciphertext, gociphertext) {
-			t.Errorf("\ngot =%X,\nwant=%X", ciphertext, gociphertext)
-		}
-		decryptPlaintext, err := DecryptOAEP(sha256.New(), nil, n, d, ciphertext, label)
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !reflect.DeepEqual(plaintext, decryptPlaintext) {
-			t.Errorf("\ngot =%X,\nwant=%X\n", plaintext, decryptPlaintext)
-		}
-	}
-}
